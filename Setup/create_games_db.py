0220b75db7f575810f8db02c019bc06010d42f8d
Paul Keffer <55495522+paulfkeffer@users.noreply.github.com>
2023-12-05 22:21:33 -0500

    Update create_games_db.py

--- Setup/create_games_db.py
+++ Setup/create_games_db.py
@@ -5,17 +5,16 @@ Python file to create the "games" database in MySQL that we will be using
 """
 
 import mysql.connector
-from configparser import ConfigParser
 import json
 
 with open("connectorConfig.json", "r") as f:
-   config = json.load(f)
+    config = json.load(f)
 
 
 connection_config = config["mysql"]
 dataBase = mysql.connector.connect(host=connection_config["host"],
-                                    user=connection_config["user"],
-                                 passwd=connection_config["passwd"])
+                                   user=connection_config["user"],
+                                   passwd=connection_config["passwd"])
 
 # Preparing a cursor object
 cursorObject = dataBase.cursor()
